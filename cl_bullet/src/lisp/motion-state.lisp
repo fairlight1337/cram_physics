e8de19dceaa6ec1d55b4905ebb15b2748f36ead5
Jan Winkler <winkler@cs.uni-bremen.de>
2015-06-24 14:36:21 +0200

    Removed extra space

--- cl_bullet/src/lisp/motion-state.lisp
+++ cl_bullet/src/lisp/motion-state.lisp
@@ -53,4 +53,4 @@
 (defmethod (setf pose) (new-value (obj motion-state))
   (set-world-transform
    (foreign-obj obj)
-   (cl-transforms:reference-transform new-value )))
+   (cl-transforms:reference-transform new-value)))
